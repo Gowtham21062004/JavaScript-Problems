3bfb7b34b8f402b622495957ff21321a562204c4
Gowtham21062004 <142236401+Gowtham21062004@users.noreply.github.com>
2024-07-24 16:40:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//rotating an array
+// const arr = [1,2,3,4,5];
+// const res = arr.map((a, i) => arr[arr.length-i-1])
+// console.log(res);
+
+
+//find
+// let str=["gowtham","RCB","CSK","MI","KKR","SRH","Gujarathtitans"];
+// let res=str.find(str=>(str.length>7));
+// console.log(res);
+
+///mappinng
+// let arr=[1,2,3,4,5,6];
+// let res=arr.map(arr=>(arr*5));
+// console.log(res);
+
+///Minimumand maximum in array
+// var arr=[23,78,34,20,18];
+// var max=0; 
+// var min=arr[0];
+// function res(arr){
+//     for(let i=0;i<arr.length;i++){
+//         if(this.max<arr[i]){
+//             this.max=arr[i];
+//         }
+//         if(this.min>arr[i]){
+//             this.min=arr[i]
+//         }
+//     }
+// }
+
+
+//missing number 
+// function missingNumber(arr) {
+    
+//     for(let i=0; i<arr.length-1; i++){
+//         if((arr[i]+1) != (arr[i+1])){
+//             return arr[i]+1;
+//         }
+//     }
+// }
+
+// let arr = [1,2,5];
+// console.log("missing numbers",missingNumber(arr));
